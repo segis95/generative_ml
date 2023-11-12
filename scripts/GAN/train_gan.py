3a160f85488f10e24eff5330d9eacdf10e4bec96
Sergey Pavlov <segis95.ai@gmail.com>
2023-11-12 17:53:21 +0100

    added logging

--- scripts/GAN/train_gan.py
+++ scripts/GAN/train_gan.py
@@ -3,10 +3,11 @@ import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-import wandb
 import hydra
+import wandb
+from wandb import Image as wandb_Image
+from wandb import Table
 from omegaconf import OmegaConf
-
 
 def load_data():
     (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
@@ -53,20 +54,22 @@ def create_generator(generator_hidden_size=100):
     
     return generator
 
+@keras.saving.register_keras_serializable()
 class GAN(keras.Model):
-    def __init__(self, discriminator, generator, hidden_size=100):
+    def __init__(self, discriminator, generator, wandb_logging=True):
         super().__init__()
-        self.generator_hidden_size = hidden_size
-        self.e2e_inp = keras.layers.Input((hidden_size,))
+        self.wandb_logging = wandb_logging
+        self.generator_hidden_size = generator.input.shape[1]
+        self.e2e_inp = keras.layers.Input((self.generator_hidden_size,))
         
         self.discriminator = discriminator
         self.generator = generator
         self.e2e_model = keras.Model(inputs=self.e2e_inp, outputs=self.discriminator(self.generator(self.e2e_inp)))
 
-    def compile(self):
+    def compile(self, disc_learning_rate, gen_learning_rate):
         super().compile()
-        self.disc_optimizer = keras.optimizers.Adam(learning_rate=0.00003)
-        self.e2e_optimizer = keras.optimizers.Adam(learning_rate=0.0001)
+        self.disc_optimizer = keras.optimizers.Adam(learning_rate=disc_learning_rate)
+        self.e2e_optimizer = keras.optimizers.Adam(learning_rate=gen_learning_rate)
         
         self.loss_e2e = keras.losses.BinaryCrossentropy()
         self.loss_disc = keras.losses.BinaryCrossentropy()
@@ -87,7 +90,6 @@ class GAN(keras.Model):
         img_real = data
         batch_size = tf.shape(img_real)[0]
 
-        
         eta = tf.random.normal(shape=(batch_size, self.generator_hidden_size), mean=0.0, stddev=1.0, dtype=tf.float32)
         img_gen = self.generator(eta)
         data_mixed = tf.concat([img_real, img_gen], axis=0)
@@ -120,41 +122,89 @@ class GAN(keras.Model):
         self.gen_loss_tracker.update_state(e2e_loss)
         self.disc_loss_tracker.update_state(disc_loss)
 
-        
         return {"disc_acc":self.acc_disc.result(), "gen_acc":self.acc_e2e.result(),   "g_loss": self.gen_loss_tracker.result(),
             "d_loss": self.disc_loss_tracker.result()}
 
-def source_data_generator(generator, n_items=100):
-    eta = np.random.normal(loc=0.0, scale=1.0, size=(n_items, generator.input.shape[1]))
-    labels = np.array([1] * n_items)
+    def get_config(self):
+        base_config = super().get_config()
+        config = {
+            "discriminator": keras.saving.serialize_keras_object(self.discriminator),
+            "generator": keras.saving.serialize_keras_object(self.generator),
+            "wandb_logging": self.wandb_logging
+        }
+        return {**base_config, **config}
+
+    @classmethod
+    def from_config(cls, config):
+        discriminator_config = config.pop("discriminator")
+        discriminator = keras.saving.deserialize_keras_object(discriminator_config)
+
+        generator_config = config.pop("generator")
+        generator = keras.saving.deserialize_keras_object(generator_config)
+
+        return cls(discriminator, generator, **config)
+
+class GeneratorProgressionCallback(keras.callbacks.Callback):
+    def __init__(self, generate_images_per_epoch):
+        super().__init__()
+        self.generate_images_per_epoch = generate_images_per_epoch
+        self.table_data = []
+
+    @staticmethod
+    def source_data_generator(generator_input_shape, images_per_epoch):
+        eta = np.random.normal(loc=0.0, scale=1.0, size=(images_per_epoch, generator_input_shape))
+        labels = np.array([1] * images_per_epoch)
+        
+        return eta, labels
+
+    def on_train_batch_end(self, batch, logs=None):
+        if self.model.wandb_logging:
+            for metric, value in logs.items():
+                wandb.log({metric: value})
     
-    return eta, labels
-    
-@hydra.main(version_base=None, config_path="../configs", config_name="train_gan_config")
+    def on_epoch_end(self, epoch, logs=None):
+        X, y = self.source_data_generator(self.model.generator.input.shape[1], self.generate_images_per_epoch)
+        generated = self.model.generator.predict(X)
+
+        fig, axes = plt.subplots(nrows=1, ncols=self.generate_images_per_epoch, figsize=(self.generate_images_per_epoch, 2))
+        plt.axis('off')
+        for ax in axes:
+            ax.set_xticks([])
+            ax.set_yticks([])
+        
+        for elid in range(self.generate_images_per_epoch):
+            axes[elid].imshow(generated[elid], cmap='gray')
+
+        self.table_data.append([epoch, wandb_Image(fig)])        
+
+    def on_train_end(self, logs=None):
+        table = Table(data=self.table_data, columns=["epoch", "examples_of_generation"])
+        wandb.log({'Generation examples from different training epochs': table})
+
+@hydra.main(version_base=None, config_path="../../configs", config_name="train_gan_config")
 def main(config):
     
-    if config.wandb.enabled:
-        wandb.config = omegaconf.OmegaConf.to_container(
+    if config.logging.wandb.enabled:
+        wandb.config = OmegaConf.to_container(
             config, resolve=True, throw_on_missing=True
         )
 
-        wandb.init(project=config.wandb.project)
+        wandb.init(project=config.logging.wandb.project)
 
     discriminator = create_discriminator()
-    generator = create_generator()
-    gan = GAN(discriminator, generator)
-    gan.compile()
+    generator = create_generator(generator_hidden_size=config.model.hidden_space_size)
+    gan = GAN(discriminator, generator, wandb_logging=config.logging.wandb.enabled)
+    gan.compile(config.training.discriminator_learning_rate, config.training.generator_learning_rate)
     images_mnist = load_data()
-    gan.fit(images_mnist, epochs=config.training.epochs, batch_size=config.training.batch_size)
+    callback = []
+    gan.fit(images_mnist, epochs=config.training.epochs,
+            batch_size=config.training.batch_size,
+            callbacks=[GeneratorProgressionCallback(config.logging.wandb.n_examples_per_epoch)])
 
-    X, y = source_data_generator(generator, n_items=100)
-    generated = generator.predict(X)
-    
-    fig, ax = plt.subplots(nrows=10, ncols=10, figsize=(30, 15))
-    for elid in range(100):
-        ax[elid // 10, elid % 10].imshow(generated[elid], cmap='gray')
-    
-    plt.savefig("gen_.png")
+    if config.logging.save_weights:
+        gan.save("gan.keras")
+
+
 
 if __name__ == '__main__':
     main()
